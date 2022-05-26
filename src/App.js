5e09448665f0d4e1200fa3e1a9fb92f6281417af
Fox, Lowella <lowella.fox@verizon.com>
2022-05-26 13:39:44 -0500

    Adding files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from "../src/pages/Home"
+import AddEditUser from '../src/pages/AddEditUser';
+import About from '../src/pages/About';
+import UserInfo from '../src/pages/UserInfo'
+import Header from "../src/components/Header"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />}/>
+          <Route path="/addUser" element={<AddEditUser />}/>
+          <Route path="/editUser/:id" element={<AddEditUser />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/userinfo/:id" element={<UserInfo />}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
---
+++ src/pages/AddEditUser.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddEditUser = () => {
+  return (
+    <div>Add Edit User</div>
+  )
+}
+
+export default AddEditUser
